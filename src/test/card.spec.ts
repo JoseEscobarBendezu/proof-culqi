3263857399df49cdf5bc74728d6d9dec1aa81621
Jose-Fedora <alvaro.escobar@unmsm.edu.pe>
2022-12-06 20:50:53 -0500

    ✨sparklesfeat(new): added get credit card feature

--- src/test/card.spec.ts
+++ src/test/card.spec.ts
@@ -1,43 +1,13 @@
+import { validToken, searchCardOnDB } from '../api/card';
+
 describe('api card lambda functions', () => {
-  test('token param is not String type', () => {
-    const expected = 'token is not valid';
-    const recibed = 'token is not valid';
-    expect(recibed).toStrictEqual(expected);
-  });
-
   test('token param is not 16 length', () => {
-    const expected = 'token length is not valid';
-    const recibed = 'token length is not valid';
-    expect(recibed).toStrictEqual(expected);
-  });
-
-  test('token is valid and dont exist in database', () => {
-    const expected = 'token is dont exist in database';
-    const recibed = {
-      card_number: '',
-      expiration_month: '',
-      expiration_year: '',
-      email: ''
-    };
-
-    expect(recibed).toEqual(expected);
-  });
-
-  test('token is valid and exist in database', () => {
-    const expected = {
-      card_number: '',
-      expiration_month: '',
-      expiration_year: '',
-      email: ''
-    };
-
-    const recibed = {
-      card_number: '',
-      expiration_month: '',
-      expiration_year: '',
-      email: ''
-    };
-
-    expect(recibed).toEqual(expected);
+    const expected = 'token is not valid';
+    expect(() => {
+      return validToken('');
+    }).toThrowError(expected);
+    expect(() => {
+      return validToken('12345678901234567');
+    }).toThrowError(expected);
   });
 });
